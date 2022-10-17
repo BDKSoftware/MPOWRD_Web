f2cb7b40b78b7b99babd92145f21936692bce7f1
Bradley Donn Kukuk <brad@bdksoftware.com>
2022-10-17 16:13:04 -0600

    Changing the new claims to change based on state values

--- src/components/NewClaimsContent.jsx
+++ src/components/NewClaimsContent.jsx
@@ -10,6 +10,8 @@ import { useRouter } from "next/router";
 
 function NewClaimsContent({ user }) {
   const [claims, setClaims] = React.useState([]);
+  const [content, setContent] = React.useState("");
+  const [selectedClaim, setSelectedClaim] = React.useState(null);
 
   const router = useRouter();
 
@@ -39,69 +41,124 @@ function NewClaimsContent({ user }) {
     getClaims();
   }, []);
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <h1 className={styles.title}>Claims</h1>
-        <div className={styles.iconContainer}>
-          <PersonIcon
-            sx={{
-              color: "#117FFF",
-              height: "16px",
-              width: "16px",
-            }}
-          />
+  const baseContent = () => {
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1 className={styles.title}>Claims</h1>
+          <div className={styles.iconContainer}>
+            <PersonIcon
+              sx={{
+                color: "#117FFF",
+                height: "16px",
+                width: "16px",
+              }}
+            />
+          </div>
         </div>
-      </div>
-      {claims.length > 0 ? (
-        <div className={styles.claimTable}>
-          <table className={styles.table}>
-            <tr className={styles.tableHeader}>
-              <th className={styles.tableLabel}>Name of Injured Worker</th>
-              <th className={styles.tableLabel}>Claim ID</th>
-              <th className={styles.tableLabel}>Date of Incident</th>
-              <th className={styles.tableLabel}>Status</th>
-              <th className={styles.tableLabel}>Actions</th>
-            </tr>
-            {claims.map((claim, index) => {
-              return (
-                <tr
-                  className={
-                    index % 2 == 0 ? styles.tableRow : styles.tableRowOdd
-                  }
-                  key={index}
-                >
-                  <td className={styles.tableItem}>
-                    {claim.nameOfInjuredWorker}
-                  </td>
-                  <td className={styles.tableItem}>{claim.id}</td>
-                  <td className={styles.tableItem}>
-                    {claim.investigatorReportDate}
-                  </td>
-                  <td className={styles.tableItem}>{claim.status}</td>
+        {claims.length > 0 ? (
+          <div className={styles.claimTable}>
+            <table className={styles.table}>
+              <tr className={styles.tableHeader}>
+                <th className={styles.tableLabel}>Name of Injured Worker</th>
+                <th className={styles.tableLabel}>Claim ID</th>
+                <th className={styles.tableLabel}>Date of Incident</th>
+                <th className={styles.tableLabel}>Status</th>
+                <th className={styles.tableLabel}>Actions</th>
+              </tr>
+              {claims.map((claim, index) => {
+                return (
+                  <tr
+                    className={
+                      index % 2 == 0 ? styles.tableRow : styles.tableRowOdd
+                    }
+                    key={index}
+                  >
+                    <td className={styles.tableItem}>
+                      {claim.nameOfInjuredWorker}
+                    </td>
+                    <td className={styles.tableItem}>{claim.id}</td>
+                    <td className={styles.tableItem}>
+                      {claim.investigatorReportDate}
+                    </td>
+                    <td className={styles.tableItem}>{claim.status}</td>
 
-                  <td className={styles.tableItem}>
-                    <div className={styles.button}>
-                      <span
-                        className={styles.buttonText}
-                        onClick={() => router.push(`/claim/${claim.id}`)}
-                      >
-                        View Claim
-                      </span>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </table>
+                    <td className={styles.tableItem}>
+                      <div className={styles.button}>
+                        <span
+                          className={styles.buttonText}
+                          onClick={() => {
+                            setContent("claims");
+                            setSelectedClaim(claim);
+                          }}
+                        >
+                          View Claim
+                        </span>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </table>
+          </div>
+        ) : (
+          <div className={styles.noClaims}>
+            <ClipLoader color={"#117fff"} loading={true} size={25} />
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  const claimContent = () => {
+    return (
+      <div className={styles.container}>
+        <div className={styles.header2}>
+          <div className={styles.headerTab}>
+            <span className={styles.headerDescription}>Name</span>
+            <span className={styles.headerValue}>
+              {selectedClaim.nameOfInjuredWorker}
+            </span>
+          </div>
+          <div className={styles.headerTab}>
+            <span className={styles.headerDescription}>Claim Number</span>
+            <span className={styles.headerValue}>{selectedClaim.id}</span>
+          </div>
+          <div className={styles.headerTab}>
+            <span className={styles.headerDescription}>Claim Manager</span>
+            <span className={styles.headerValue}>
+              {selectedClaim.claimManager === ""
+                ? "Unassigned"
+                : selectedClaim.claimManager}
+            </span>
+          </div>
+          <div className={styles.headerTab}>
+            <span className={styles.headerDescription}>Inception Date</span>
+            <span className={styles.headerValue}>
+              {selectedClaim.inceptionDate}
+            </span>
+          </div>
+          <div className={styles.headerRightContainer}>
+            <div className={styles.buttonContainer}>
+              <span className={styles.buttonText2}> Start a Claim</span>
+            </div>
+            <div className={styles.iconContainer}>
+              <PersonIcon
+                sx={{
+                  color: "#117FFF",
+                  height: "16px",
+                  width: "16px",
+                }}
+              />
+            </div>
+          </div>
         </div>
-      ) : (
-        <div className={styles.noClaims}>
-          <ClipLoader color={"#117fff"} loading={true} size={25} />
-        </div>
-      )}
-    </div>
-  );
+        <div className={styles.bottomContainer}></div>
+      </div>
+    );
+  };
+
+  return content == "" ? baseContent() : claimContent();
 }
 
 export default NewClaimsContent;
